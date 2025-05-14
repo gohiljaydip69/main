63334a355ea5eea4496d31cfecfb4ee260987d63
jaydip <gohiljaydip69@gmail.com>
2025-05-14 20:05:08 +0530

    social media link add

--- src/components/contact/contact.jsx
+++ src/components/contact/contact.jsx
@@ -13,16 +13,16 @@ const Contact = () => {
    
       <div className="contact-container">
         <div className="contact-item">
-          <div className="contact-sub-item">
+          <div className="contact-sub-item" onClick={() => window.open("https://github.com/gohiljaydip69")}>
             <FaGithub className="contact-item-inner" /> <p>@jaydipgohil69</p>
           </div>
           
-          <div className="contact-sub-item">
+          <div className="contact-sub-item" onClick={() => window.open("https://www.linkedin.com/in/jaydipgohil31/")}>
             <CiLinkedin className="contact-item-inner" />
             <p>@jaydipgohil31</p>
           </div>
 
-          <div className="contact-sub-item">
+          <div className="contact-sub-item" onClick={() => window.open("mailto:gohiljaydip69@gmail.com")}>
             <MdOutlineMailOutline className="contact-item-inner" />
             <p>gohiljaydip69@gmail.com</p>
           </div>
